1d680f6d843a47fddc717efb7e91bd8adaec5939
MakwanaRajesh <149988396+MakwanaRajesh@users.noreply.github.com>
2024-03-29 09:25:03 +0530

    Add files via upload

---
+++ Lab6_6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Lab6_6 {
+	public static void main(String[] args) {
+		Child c = new Child();
+		c.display();
+		sc.close();
+	}
+}
+
+class Parent {
+	public void display() {
+		System.out.println("Parent Class");
+	}
+}
+
+class Child extends Parent {
+	public void display() {
+		super.display();
+		System.out.println("Child Class");
+	}
+
+}
